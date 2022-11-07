9fc1be48f9a5942d8e0b8a2023747af615c934ce
Paul GAUTHIER <paul.gauthier@grenoble-inp.org>
2022-11-07 19:47:22 +0100

    discord bot et data

---
+++ TestDiscordBot.py
@@ -0,0 +1,22 @@
+import discord
+
+TOKEN = "MTAzOTIyOTc5Njk2Nzc4ODY4NA.GEBYR2.bQWmWep-AqaeP8YKBZoWNzVp8AlXZf6z089BRk"
+
+messages = "Mon bot vient de dire quelque chose..."
+
+client = discord.Client(intents=discord.Intents.default())
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+    channel = client.get_channel(1039230555797061704)
+    await channel.send(messages)
+
+    user = await client.fetch_user(317684843393581056)
+    await user.send(messages)
+
+    await client.close()
+    time.sleep(1)
+
+
+client.run(TOKEN)
