44396156027c38cd9b942dd451b6d9c106119613
Paul GAUTHIER <paul.gauthier@grenoble-inp.org>
2023-02-10 20:04:26 +0100

    modif test discord

--- TestDiscordBot.py
+++ TestDiscordBot.py
@@ -9,14 +9,19 @@ client = discord.Client(intents=discord.Intents.all())
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
     
+    global general
+    general = client.get_channel(1039230555797061704)
+    
     global botChannel
-    botChannel = client.get_channel(1039230555797061704)
+    botChannel = client.get_channel(1041048235378225192)
     
     global simon
     simon = await client.fetch_user(316949626697678858)
 
     global paul
     paul = await client.fetch_user(317684843393581056)
+    
+    await botChannel.send(":green_circle: \n ca marche")
 
 
 @client.event
